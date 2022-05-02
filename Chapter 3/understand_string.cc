dc7592bdb4a8916da8ce7320a3c2187f12d35dd9
Xiaoliang Wu <zurielibra@gmail.com>
2022-05-01 20:11:40 -0700

    add understand_string.cc

---
+++ Chapter 3/understand_string.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string> 
+
+using std::string;
+
+int main(){
+    string s1 = "hello";
+    // string s2 = "hello" + "world"; // error: can't add string literals
+    for (auto c : s1)
+        std::cout << c << std::endl;
+    string s2; 
+    std::cin >> s2; // try to type "12 34"
+    std::cout << s2 << std::endl; // output "12"
+    for (auto &c : s1)
+        c = 'X';
+    std::cout << s1 << std::endl; // output "XXXXX"
+}
